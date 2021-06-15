642e1f069d3e955c9c61464b23425fa0568aaa56
kaZahiX <janekerenfeld@interia.pl>
2021-06-15 19:02:16 +0200

    Gra dziala, ale nie dziala

--- gra_helikopter/gra.py
+++ gra_helikopter/gra.py
@@ -1,4 +1,7 @@
+import random
+
 import pygame
+import os
 
 pygame.init()
 
@@ -14,12 +17,45 @@ def napisz(tekst, rozmiar):
     y = (wys - rend.get_rect().height) / 2
     screen.blit(rend, (x, y))
 
+
+class Przeszkoda:
+    def __init__(self, x, szerokosc):
+        self.x = x
+        self.szerokosc = szerokosc
+        self.y_gora = 0
+        self.wys_gora = random.randint(150, 250)
+        self.odstep = 200
+        self.y_dol = self.wys_gora
+        self.wys_dol = wys - self.y_dol
+        self.kolor = (160, 140, 190)
+        self.ksztalt_gora = pygame.Rect(self.x, self.y_gora, self.szerokosc, self.wys_gora)
+        self.ksztalt_dol = pygame.Rect(self.x, self.y_dol, self.szerokosc, self.wys_dol)
+
+    def rysuj(self):
+        #pygame.draw.rect(screen, self.kolor, self.ksztalt_gora, 0)
+        pygame.draw.rect(screen, self.kolor,  self.ksztalt_dol, 0)
+
+    def ruch(self, v):
+        self.x = self.x - v
+        self.ksztalt_gora = pygame.Rect(self.x, self.y_gora, self.szerokosc, self.wys_gora)
+
+
+
+copokazuje = "rozgrywka"
+przeszkody = []
+for i in range(21):
+    przeszkody.append(Przeszkoda(i * szer / 20, szer / 20))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-
-    napisz("Naciśnij spacje, aby zacząć", 20)
+    if copokazuje == "menu":
+        napisz("Naciśnij spacje, aby zacząć", 20)
+        grafika = pygame.image.load(os.path.join("helikopter.png"))
+        screen.blit(grafika, (20, 80))
+    elif copokazuje == "rozgrywka":
+        for p in przeszkody:
+            p.rysuj()
     pygame.display.update()
-
